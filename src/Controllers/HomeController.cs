c80dcf506dd196526991b57a977885efc7f7b5f3
Jon Galloway <jongalloway@gmail.com>
2017-07-23 00:08:19 -0700

    Added initial website source

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace DotNetFoundationWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger)
+        {
+            log = logger;
+        }
+
+        private ILogger log;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+
+    }
+}
