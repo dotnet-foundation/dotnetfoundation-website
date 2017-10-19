89a8baa9a2745467d2afd689ce5b32ef93dd2b03
Jon Galloway <jongalloway@gmail.com>
2017-10-19 14:14:23 -0700

    Added initial NewsViewComponent

---
+++ src/ViewComponents/NewsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetfoundation.Services;
+using dotnetfoundation.Models;
+
+namespace dotnetfoundation.ViewComponents
+{
+    public class NewsViewComponent : ViewComponent
+    {
+        private readonly NewsFeedService newsService;
+
+        public NewsViewComponent(NewsFeedService newsService)
+        {
+            this.newsService = newsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int quantity)
+        {
+            var feed = await newsService.GetFeed();
+            var items = feed.Items.ToList().Take(quantity);
+            return View(items);
+        }
+    }
+}
