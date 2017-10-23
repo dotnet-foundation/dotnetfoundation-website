5a28179793d9ee352bca7ed9e561c1dd898a65ad
Jon Galloway <jongalloway@gmail.com>
2017-10-23 01:46:02 -0700

    Implemented Meetup ViewComponent

---
+++ src/ViewComponents/MeetupViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetfoundation.Services;
+using dotnetfoundation.Models;
+
+namespace dotnetfoundation.ViewComponents
+{
+    public class MeetupViewComponent : ViewComponent
+    {
+        private readonly MeetupFeedService meetupService;
+
+        public MeetupViewComponent(MeetupFeedService meetupService)
+        {
+            this.meetupService = meetupService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int quantity)
+        {
+            var feed = await meetupService.GetFeed();
+            var items = feed.Events.Take(quantity);
+
+            return View(items);
+        }
+    }
+}
