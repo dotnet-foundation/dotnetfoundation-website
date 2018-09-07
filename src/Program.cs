30b30986a51f3b218290b302bfe9eb0f3af22c08
Jon Galloway <jongalloway@gmail.com>
2018-09-07 12:13:43 -0700

    Roll back config changes

--- src/Program.cs
+++ src/Program.cs
@@ -114,7 +114,6 @@ namespace DotNetFoundationWebsite
             }
 
             loggerFactory.AddDbLogger(serviceProvider, logFilter);
-            loggerFactory.AddAzureWebAppDiagnostics();
         }
 
     }
