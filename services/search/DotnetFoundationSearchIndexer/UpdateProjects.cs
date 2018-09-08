e0372ba817dd39fa5aefdd21e9faab9e9c7eb743
Jon Galloway <jongalloway@gmail.com>
2018-09-07 17:34:45 -0700

    Testing post webhook

--- services/search/DotnetFoundationSearchIndexer/UpdateProjects.cs
+++ services/search/DotnetFoundationSearchIndexer/UpdateProjects.cs
@@ -22,7 +22,7 @@ namespace DotnetFoundationSearchIndexer
         private static HttpClient httpClient = new HttpClient();
         [FunctionName("UpdateProjects")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, WebHookType = "github")]HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post")]HttpRequest req,
             ILogger log)
         {
             var indexName = Env("SEARCH_INDEX_NAME");
