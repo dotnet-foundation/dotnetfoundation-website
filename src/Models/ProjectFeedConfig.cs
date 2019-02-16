801e4ee23bc72b1c20b69b068e11d68067d687dd
NextTurn <45985406+NextTurn@users.noreply.github.com>
2019-02-16 00:00:00 +0000

    Update repo names

--- src/Models/ProjectFeedConfig.cs
+++ src/Models/ProjectFeedConfig.cs
@@ -8,7 +8,7 @@ namespace dotnetfoundation.Models
     public class ProjectFeedConfig
     {
         public string RepoFeedUrl { get; set; } = "https://dnfrepos.blob.core.windows.net/output/projects_all.json";
-        public string ProjectFeedUrl { get; set; } = "https://raw.githubusercontent.com/dotnet/home/master/projects/projects.json";
+        public string ProjectFeedUrl { get; set; } = "https://raw.githubusercontent.com/dotnet/foundation/master/projects/projects.json";
         public string RepoFeedCacheKey { get; set; } = "projRepoFeed";
         public string ProjectFeedCacheKey { get; set; } = "projFeed";
         public int CacheDurationInSeconds { get; set; } = 36000; //10 hours
